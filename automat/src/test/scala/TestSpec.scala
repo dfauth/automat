26b49b167c66a51d431f204eaf821812c33449fb
Damian Fauth <dfauth@gmail.com>
2018-08-29 08:35:13 +0800

    gievn/when/then

--- automat/src/test/scala/TestSpec.scala
+++ automat/src/test/scala/TestSpec.scala
@@ -1,14 +1,17 @@
-import io.restassured.RestAssured.given
+import io.restassured.RestAssured.{given, when}
 import org.apache.logging.log4j.scala.Logging
 import org.scalatest._
 
 class TestSpec extends FlatSpec with Matchers with Logging {
 
     "Json serialization" should "work" in {
-      logger.info("in the test")
-      val response = given().port(9000).get("http://localhost:9000/api/state/identity")
-      logger.info("response: "+response)
-      response.statusCode() should be (403)
+      given.
+
+      when.
+        get("http://localhost:9000/api/state/identity").
+
+      then().
+        statusCode(403)
     }
 }
 
